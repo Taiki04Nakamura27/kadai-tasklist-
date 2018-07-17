27904af8492ae0dbfffacc2b1c7770675272fe4b
Taiki.Nakamura <taiki3detail@gmail.com>
2018-07-17 11:45:16 +0000

    show.blade.php修正

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,7 +4,7 @@
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
 
-    <p>進捗状況: {{ $task->content }}</p>
+    <p>進捗状況: {{ $task->status }}</p>
     <p>タスク内容:{{ $task->content }}</p>
 
     {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
