d4e5ef37dd22dedde36b023815eb78e83c357afd
Taiki.Nakamura <taiki3detail@gmail.com>
2018-07-18 02:16:43 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,17 +3,20 @@
 @section('content')
 
 <h1>タスク新規作成ページ</h1>
-
+<div class="row">
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
-
+    <div class="form-group">
         {!! Form::label('status', '進捗状況:') !!}
-        {!! Form::text('status') !!}
-
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
         {!! Form::label('content', 'タスク内容:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('保存') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+    </div>
+        {!! Form::submit('保存',['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
-
+    </div>
+</div>
 @endsection
