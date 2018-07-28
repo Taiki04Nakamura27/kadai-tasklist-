c99e5c2bd411a327a26443083f28df7883bf8109
Taiki.Nakamura <taiki3detail@gmail.com>
2018-07-28 18:16:55 +0000

    tasklisedit20180729_error

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('tasks.index', ['tasks' => $tasks])
+
+<h1>タスク一覧</h1>
+
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>進捗状況</th>
+                    <th>内容</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                    <tr>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                        <td>{{ $task->status }}</td>
+                        <td>{{ $task->content }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    {!! link_to_route('tasks.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
 @endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('tasks.show', ['tasks' => $tasks])
+
+    <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
+
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>進捗状況</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+        <tr>
+            <th>タスク内容</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
+
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
+
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除',['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+
 @endsection
